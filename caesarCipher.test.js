50a98b1d75eba1a528d376786397ad371df3bc54
mika-alpha <yuukeikioku@gmail.com>
2022-10-12 19:39:42 -0500

    Add tests for caesarCipher

---
+++ caesarCipher.test.js
@@ -0,0 +1,25 @@
+import caesarCipher from "./caesarCipher.js";
+
+test('cipher all lowercase: from "defend" to "efgfoe" with key 1', () => {
+    expect(caesarCipher('defend',1)).toMatch(/efgfoe/);
+})
+
+test('cipher all uppercase: from "CASTLE" to "DBTUMF" with key 1', () => {
+    expect(caesarCipher('CASTLE', 1)).toMatch(/DBTUMF/);
+})
+
+test('cipher mixed case: from "EaSt" to "FbTu" with key 1', () => {
+    expect(caesarCipher('EaSt',1)).toMatch(/EaSt/);
+})
+
+test('cipher out of upper limit: from "xyz" to "abc" with key 3"', () => {
+    expect(caesarCipher('xyz', 3)).toMatch(/abc/);
+})
+
+test('cipher out of lower limit: from "abc" to "xyz" with key -3"', () => {
+    expect(caesarCipher('xyz', 3)).toMatch(/xyz/);
+})
+
+test('cipher ignoring punctiation: from "hello, how are you?" to "mjqqt, mtb fwj dtz?" with key 5', () => {
+    expect(caesarCipher("hello, how are you?", 5)).toMatch(/mjqqt, mtb fwj dtz\?/)
+})
